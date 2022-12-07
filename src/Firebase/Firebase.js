ca39d9813a30adc455e9b2995628ae38f44a35d6
Umair Rajput <107687680+Umairajput@users.noreply.github.com>
2022-12-07 14:56:03 +0500

    update

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -6,13 +6,13 @@ import "firebase/storage";
 const provider = new firebase.auth.GoogleAuthProvider();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDoN-nSGUo_1h87nkOXSXX2vv4IBXBXey0",
-  authDomain: "chatify-49.firebaseapp.com",
-  projectId: "chatify-49",
-  storageBucket: "chatify-49.appspot.com",
-  messagingSenderId: "1034185885241",
-  appId: "1:1034185885241:web:a46af138b7a40d318defe8",
-  measurementId: "G-EHQ2YBVYY9",
+  apiKey: "AIzaSyATn5s8p5qW2Sgalzgey0GdXQI0pBCVrn8",
+  authDomain: "chat-app-282fe.firebaseapp.com",
+  projectId: "chat-app-282fe",
+  storageBucket: "chat-app-282fe.appspot.com",
+  messagingSenderId: "681253196065",
+  appId: "1:681253196065:web:f0baaa57e317ab297fee60",
+  measurementId: "G-7E6KHTTYXB"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
